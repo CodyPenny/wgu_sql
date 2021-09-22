5ad72c574955756612be1375b021a3489247967b
evelyn <evelynrhee103@gmail.com>
2021-09-22 11:55:04 -0700

    add read me

--- src/wgu_full/Controller.java
+++ src/wgu_full/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package wgu_full;
 
 public class Controller {
 }
--- src/wgu_full/Main.java
+++ src/wgu_full/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package wgu_full;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
