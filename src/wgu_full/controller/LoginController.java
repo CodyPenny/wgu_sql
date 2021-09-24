6afdfb42bb44b7d855845efa8db5ed1e3c36bc4f
evelyn <evelynrhee103@gmail.com>
2021-09-23 22:06:04 -0700

    translates form to french

--- src/wgu_full/controller/LoginController.java
+++ src/wgu_full/controller/LoginController.java
@@ -2,10 +2,12 @@ package wgu_full.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.time.ZoneId;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
@@ -13,14 +15,20 @@ import java.util.ResourceBundle;
  */
 public class LoginController implements Initializable {
 
+    private String system_language = "";
+
     /**
-     * Zone label
+     * labels
      */
-    @FXML private Label zoneLabel;
-
+    @FXML private Label zoneLabel, nameLabel, passwordLabel, companyLabel, errorLabel;
 
     /**
-     * Sets a label on the login form with the user's location
+     * buttons
+     */
+    @FXML private Button loginBtn;
+
+    /**
+     * Sets the zone label on the login form with the user's location
      *
      * @param zone the zone or the user's location
      */
@@ -28,8 +36,19 @@ public class LoginController implements Initializable {
         zoneLabel.setText(zone);
     }
 
+    public void translate(){
+        system_language = Locale.getDefault().toString();
+        if( system_language.equals("fr")){
+            nameLabel.setText("Nom d'utilisateur");
+            passwordLabel.setText("Mot de passe");
+            loginBtn.setText("Connexion");
+        }
+    }
+
     /**
      * Initializes the controller after its root element processes
+     * Sets the zone id with the user's location
+     * Translates the form to French if user's system is set to French
      *
      * @param url The location used to resolve relative paths for the root object, or null if the location is not known.
      * @param resourceBundle The resources used to localize the root object, or null if the root object was not localized.
@@ -38,5 +57,8 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ZoneId zone = ZoneId.systemDefault();
         setZoneLabel(zone.toString());
+        //test French
+        Locale.setDefault(new Locale("fr"));
+        translate();
     }
 }
